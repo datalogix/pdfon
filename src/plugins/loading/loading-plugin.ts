240ee19ac043bc1edd72c96c04328fa5bc393a86
Ricardo Gobbo de Souza <ricardogobbosouza@yahoo.com.br>
2025-01-23 17:01:45 -0300

    fix: loading percentage

--- src/plugins/loading/loading-plugin.ts
+++ src/plugins/loading/loading-plugin.ts
@@ -8,7 +8,11 @@ export class LoadingPlugin extends Plugin {
     this.loadingService = new LoadingService(this.rootContainer, this.l10n)
 
     this.on('documentopen', () => this.loadingService?.init())
-    this.on('documentprogress', ({ loaded, total }) => this.loadingService?.update(loaded, total))
+    this.on('documentprogress', ({ loaded, total }) => {
+      console.log(loaded, total)
+      console.log(typeof loaded, typeof total)
+      this.loadingService?.update(parseInt(loaded, 0), parseInt(total, 0))
+    })
     this.on('documentloaded', () => this.loadingService?.finish())
     this.on(['documentinitialized', 'documenterror', 'documentempty'], () => this.loadingService?.complete())
   }
